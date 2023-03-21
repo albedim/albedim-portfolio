bcb4b40005173dec36eb5477276f83909da75a2a
albedim <dimaio.albe@gmail.com>
2023-03-21 12:56:52 +0100

    1.0.5 - Works was added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 
-// Application
-
 function App() {
   return (
     <BrowserRouter>
