e2a4f4684d84770fe55385f291c958f953fa2229
git hub user name <=>
2023-03-21 12:52:43 +0100

    1.0.5 - Works was added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 
+// Application
+
 function App() {
   return (
     <BrowserRouter>
