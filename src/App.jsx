0be12519b5dbc318f6564d7db294d43133364823
albedim <dimaio.albe@gmail.com>
2023-03-28 19:24:10 +0200

    1.4.0 - Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 
+
 function App() {
   return (
     <BrowserRouter basename='/albedim-portfolio'>
